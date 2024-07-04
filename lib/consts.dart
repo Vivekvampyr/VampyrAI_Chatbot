16d3dbe43f613c85d579e3466c0421affea60556
VAMPYR <rajawatshivam55@gmail.com>
2024-07-04 13:45:10 +0530

    commit_chatbot_01

---
+++ lib/consts.dart
@@ -0,0 +1,1 @@
+const String GEMINI_API_KEY = 'AIzaSyDc5eb24VWqq2MqqVeEBlAu8Ch_HoUeRn0';
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:gemini_ai_chatbot/consts.dart';
+import 'package:gemini_ai_chatbot/home_page.dart';
+
+void main() {
+  Gemini.init(apiKey: GEMINI_API_KEY);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
